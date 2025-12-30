10316323bd2e38658390486dd4f8a70e5bad5935
wn <45404661+ob-xyz@users.noreply.github.com>
2025-12-30 16:51:23 -0500

    1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -45,7 +45,7 @@ export default function Index() {
         </div>
       </div>
       <div className="inner-content2">
-      <h2>Newsletters meticulously designed to <b>elevate your life</b> just a little bit more</h2>
+      <h2>Newsletters designed to <b>elevate your life</b> just a little more</h2>
       <a title="subscription form" href="https://jeffamazon.com/subscribe"  rel="noopener noreferrer">
       <p>Shilll helps you scroll less and live more. Evevate your life with our 1-minute newsletters, meticulously designed and highly curated to be quick, informative, and awfully fun.</p>
       </a>
