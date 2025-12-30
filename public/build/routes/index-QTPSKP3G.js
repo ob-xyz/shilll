f757d8c6a17f70803610549022a83c8e3751b99f
wn <45404661+ob-xyz@users.noreply.github.com>
2025-12-29 23:44:31 -0500

    1

--- public/build/routes/index-QTPSKP3G.js
+++ public/build/routes/index-QTPSKP3G.js
@@ -118,7 +118,7 @@ function Index() {
     /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-content2", children: [
       /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h2", { children: [
         "We make newsletters that are ",
-        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("span", { children: "enjoyable" }, void 0, false, {
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("span", { children: "actually enjoyable" }, void 0, false, {
           fileName: "app/routes/index.tsx",
           lineNumber: 48,
           columnNumber: 40
@@ -222,4 +222,4 @@ function Index() {
 export {
   Index as default
 };
-//# sourceMappingURL=/build/routes/index-5P26DE55.js.map
+//# sourceMappingURL=/build/routes/index-QTPSKP3G.js.map
