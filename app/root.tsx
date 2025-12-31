8e81364e406657aaa2bc35bc6b4b4e2a5305c637
wn <45404661+ob-xyz@users.noreply.github.com>
2025-12-31 15:48:00 -0500

    1

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll",
-  description: "Live More. Do More.",
+  description: "Scroll Less. Live More.",
   viewport: "width=device-width,initial-scale=1"
 });
 
