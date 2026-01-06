ab7713098213bf23d63326d4b237e11af6ac9632
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-06 17:25:11 -0500

    1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -52,7 +52,7 @@ export default function Index() {
       <div className="inner-content2">
       <h2>The future of media is <span>bespoke</span></h2>
       <a title="subscription form" href="https://jeffamazon.com/subscribe"  rel="noopener noreferrer">
-      <p>Shilll replaces infinite scrolling with intentional insights. Our 1-minute newsletters are meticulously designed to empower the minds of individuals wanting to scroll less and live more.</p>
+      <p>Shilll replaces infinite scrolling with intentional insights. Our bespoke 1-minute newsletters are meticulously designed to empower the minds of individuals across the world.</p>
       </a>
       </div>
         <div className="floatimg">
