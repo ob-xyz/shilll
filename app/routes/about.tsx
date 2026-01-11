2b2a88359bc03360815f0a2ee7a1bfbefb600e1f
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-10 19:57:21 -0500

    1

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -49,7 +49,7 @@ export default function Index() {
         <h1>Jeffamazon</h1>
         <p>Daily business news</p>
       <div className="btn">
-          <Link className="pricebtn" to="/subscribe">Subscribe for free</Link>
+          <a className="pricebtn" href="https://jeffamazon.com/subscribe">Subscribe for free</a>
       </div>
       </div>
       </div>
@@ -61,7 +61,7 @@ export default function Index() {
         <h1>The Poast</h1>
         <p>Weekly local news</p>
       <div className="btn">
-          <Link className="pricebtn" to="/subscribe">Subscribe for free</Link>
+          <a className="pricebtn" href="https://thepoast.com/subscribe">Subscribe for free</a>
       </div>
       </div>
      </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -48,7 +48,7 @@ export default function Index() {
         <h1>Jeffamazon</h1>
         <p>Daily business news</p>
       <div className="btn">
-          <Link className="pricebtn" to="/subscribe">Subscribe for free</Link>
+          <a className="pricebtn" href="https://jeffamazon.com/subscribe">Subscribe for free</a>
       </div>
       </div>
       </div>
@@ -60,7 +60,7 @@ export default function Index() {
         <h1>The Poast</h1>
         <p>Weekly local news</p>
       <div className="btn">
-          <Link className="pricebtn" to="/subscribe">Subscribe for free</Link>
+          <a className="pricebtn" href="https://thepoast.com/subscribe">Subscribe for free</a>
       </div>
       </div>
      </div>
