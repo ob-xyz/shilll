6637d3f101d1d62e3d9a7537334d3a9b32e19811
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-27 12:33:11 -0500

    11

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
       <div className="content">
         <div className="inner-content">
           <div className="text">
-            <h1>Don't waste the best years of your career.</h1>
+            <h1>Don't waste the best years of your career</h1>
             <p>Mix your compensation to fit your goals. <br />Get immediate vesting. Zero cliffs.<br />Start maximizing your career.</p>
           </div>
         <div className="btn">
