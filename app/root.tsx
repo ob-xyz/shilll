d28a4dc3680a4415f6f7d4a98e2da6359368bab0
wn <45404661+ob-xyz@users.noreply.github.com>
2026-02-24 21:16:55 -0500

    112

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll : Shine bright",
-  description: "Shilll is a human-centric AI company.",
+  description: "Shilll makes tools that adapt to you.",
   viewport: "width=device-width,initial-scale=1"
 });
 
