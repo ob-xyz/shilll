7a7eaf678f4ca6f9f3735b20a4ebabcec94bef2f
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-03 23:27:24 -0500

    1122

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll",
-  description: "Is this thing on?",
+  description: "Get real",
   viewport: "width=device-width,initial-scale=1"
 });
 
