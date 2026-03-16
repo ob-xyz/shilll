28a60995c4dd040b06540a31ce89bfea19233ff4
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-16 10:47:27 -0400

    1122

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll",
-  description: "The future is yours to discover",
+  description: "We don't wait for greatness. We make it.",
   viewport: "width=device-width,initial-scale=1"
 });
 
