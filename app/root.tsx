53ffed2f92f8b9793d1a7de2b815bfc412914551
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-25 15:51:49 -0400

    1

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,6 @@ export const meta: MetaFunction = () => ({
   description: "We make technology more personal.",
   viewport: "width=device-width,initial-scale=1"
 });
-
 export default function App() {
 
   
