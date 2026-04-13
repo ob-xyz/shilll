e82767af292db1040f0dfa1d3c30f181b38ddc28
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-13 05:40:49 -0400

    1

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll : Shilll",
-  description: "We make intelligence more personal.",
+  description: "We design and build things.",
   viewport: "width=device-width,initial-scale=1"
 });
 export default function App() {
