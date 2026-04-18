aa46ecb1de28bdf0e090068aff1a28222709afa7
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-18 12:29:44 -0400

    Revert "1"
    This reverts commit e82767af292db1040f0dfa1d3c30f181b38ddc28.

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll : Shilll",
-  description: "We design and build things.",
+  description: "We make intelligence more personal.",
   viewport: "width=device-width,initial-scale=1"
 });
 export default function App() {
