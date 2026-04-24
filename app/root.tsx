469e7f046072ecfe943a34c264594322e230f442
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-24 16:11:38 -0400

    11

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Shilll — Scaling Software",
+  title: "Shilll",
   description: "We love to make software.",
   viewport: "width=device-width,initial-scale=1"
 });
