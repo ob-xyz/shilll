7d109deb3b2a11bb7ac6fdc5102e3dda96b0a493
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-28 14:31:31 -0400

    1

--- app/routes/profile/personal.tsx
+++ app/routes/profile/personal.tsx
@@ -22,7 +22,7 @@ export default function Index() {
                 <button className="submit" type="submit">Submit</button>
               </div>
               <Altcha />
-               <input id="82687" type="hidden" name="l" checked value="82687238-ae68-46c7-98ad-183fbf4cfea0" />
+              <input id="bcd05" type="hidden" name="l" checked value="bcd05274-b4b9-4a90-a8b5-1fdee92637fa" />
               <input type="hidden" name="nonce" />
             </form>
         </div>
