2c01d3c5f2686df90d88f994ac671d20101e0724
wn <45404661+ob-xyz@users.noreply.github.com>
2026-05-10 18:01:11 -0400

    11

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shilll — gotta minute?",
-  description: "Check out our one minute news feeds.",
+  description: "Subscribe to our free, one minute news feeds.",
   viewport: "width=device-width,initial-scale=1"
 });
 export default function App() {
